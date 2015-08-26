6fedd84fb8e7b3cfccb604f296e94e9a7dde2d08
PaoloGallina <pg1992@hotmail.it>
2015-08-26 18:08:24 +0200

    Unito lo stub dell'hash

---
+++ FunzioneMain/FunzioneMain/HashFiles.h
@@ -0,0 +1,6 @@
+
+
+#include "stdafx.h"
+#include <string>
+
+std::string ComputeHash();
--- HashFiles/HashFiles/HashFiles.cpp
+++ HashFiles/HashFiles/HashFiles.cpp
@@ -1,5 +1,4 @@
-// HashFiles.cpp : Defines the entry point for the console application.
-//
+
 
 #include "stdafx.h"
 #include "Sha.h"
@@ -11,24 +10,14 @@
 using namespace std;
 
 
-int _tmain(int argc, _TCHAR* argv[])
+string ComputeHash()
 {
-
 	std::ifstream ifs("C:\\Users\\Paolo\\Desktop\\PROVA2\\Hydrangeas.jpg",std::ios::binary);
 	std::ostringstream sstringa;
 	sstringa << ifs.rdbuf();
-	string stringa(sstringa.str());
-	string output1 = sha256(stringa);
 	ifs.close();
-	
-	
-	//This part has the only pourpose to check that the string is the right one.
-	//std::ofstream ofs("C:\\Users\\Paolo\\Desktop\\PROVA2\\ARRR.jpg", std::ios::binary);
-	//ofs << stringa;
-	//cout << "sha256('" << STRINGAENORME.str().length()<< "'):" << output1 << endl;
-	//ofs.close();
-	
-	return 0;
+	string stringa(sstringa.str());
 
+	return sha256(stringa);
 }
 
