735f9e470eb2220496decd730aaa8b1ba969d298
PaoloGallina <pg1992@hotmail.it>
2015-09-29 23:21:31 +0200

    GUI login almost done

--- PrimaGUI/PrimaGUI/Form2.Designer.cs
+++ PrimaGUI/PrimaGUI/Form2.Designer.cs
@@ -31,8 +31,6 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form2));
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
@@ -41,15 +39,23 @@
             this.button5 = new System.Windows.Forms.Button();
             this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
             this.label4 = new System.Windows.Forms.Label();
+            this.Help = new System.Windows.Forms.Button();
+            this.About = new System.Windows.Forms.Button();
+            this.infoLabel = new System.Windows.Forms.Label();
+            this.portatext = new System.Windows.Forms.TextBox();
+            this.iptext = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.DodgerBlue;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button1.Font = new System.Drawing.Font("Arial", 12.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(199, 35);
+            this.button1.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.button1.Location = new System.Drawing.Point(90, 246);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(153, 68);
+            this.button1.Size = new System.Drawing.Size(121, 42);
             this.button1.TabIndex = 0;
             this.button1.Text = "Login";
             this.button1.UseVisualStyleBackColor = false;
@@ -57,59 +63,44 @@
             // 
             // button2
             // 
-            this.button2.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.button2.BackColor = System.Drawing.Color.Transparent;
+            this.button2.FlatAppearance.BorderColor = System.Drawing.Color.DodgerBlue;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button2.Font = new System.Drawing.Font("Arial", 12.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(199, 153);
+            this.button2.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.button2.Location = new System.Drawing.Point(89, 168);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(153, 68);
+            this.button2.Size = new System.Drawing.Size(122, 42);
             this.button2.TabIndex = 1;
             this.button2.Text = "Registrati";
             this.button2.UseVisualStyleBackColor = false;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.label1.Font = new System.Drawing.Font("Arial", 12.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(69, 60);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(102, 19);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "Nome Utente";
-            this.label1.Visible = false;
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.BackColor = System.Drawing.Color.Transparent;
-            this.label2.Font = new System.Drawing.Font("Arial", 12.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(69, 134);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(80, 19);
-            this.label2.TabIndex = 3;
-            this.label2.Text = "Password";
-            this.label2.Visible = false;
-            // 
             // textBox1
             // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.InactiveBorder;
             this.textBox1.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.Location = new System.Drawing.Point(199, 54);
+            this.textBox1.ForeColor = System.Drawing.SystemColors.InactiveCaption;
+            this.textBox1.Location = new System.Drawing.Point(11, 168);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(153, 25);
+            this.textBox1.Size = new System.Drawing.Size(278, 25);
             this.textBox1.TabIndex = 4;
+            this.textBox1.Text = "Username";
             this.textBox1.Visible = false;
+            this.textBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.textBox1_TextChanged);
             // 
             // textBox2
             // 
-            this.textBox2.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox2.Location = new System.Drawing.Point(199, 128);
+            this.textBox2.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.textBox2.Font = new System.Drawing.Font("Arial", 10.25F);
+            this.textBox2.ForeColor = System.Drawing.SystemColors.InactiveCaption;
+            this.textBox2.Location = new System.Drawing.Point(11, 206);
             this.textBox2.Name = "textBox2";
-            this.textBox2.PasswordChar = 'x';
-            this.textBox2.Size = new System.Drawing.Size(153, 25);
+            this.textBox2.Size = new System.Drawing.Size(278, 23);
             this.textBox2.TabIndex = 5;
-            this.textBox2.UseSystemPasswordChar = true;
+            this.textBox2.Text = "Password";
             this.textBox2.Visible = false;
+            this.textBox2.MouseClick += new System.Windows.Forms.MouseEventHandler(this.textBox2_MouseClick);
             // 
             // label3
             // 
@@ -124,11 +115,13 @@
             // 
             // button3
             // 
-            this.button3.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.button3.BackColor = System.Drawing.Color.Transparent;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button3.Font = new System.Drawing.Font("Arial", 12.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button3.Location = new System.Drawing.Point(381, 200);
+            this.button3.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.button3.Location = new System.Drawing.Point(154, 391);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(153, 68);
+            this.button3.Size = new System.Drawing.Size(137, 27);
             this.button3.TabIndex = 7;
             this.button3.Text = "OK";
             this.button3.UseCompatibleTextRendering = true;
@@ -138,11 +131,13 @@
             // 
             // button4
             // 
-            this.button4.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.button4.BackColor = System.Drawing.Color.Transparent;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button4.Font = new System.Drawing.Font("Arial", 12.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button4.Location = new System.Drawing.Point(9, 200);
+            this.button4.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.button4.Location = new System.Drawing.Point(9, 391);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(153, 68);
+            this.button4.Size = new System.Drawing.Size(137, 27);
             this.button4.TabIndex = 8;
             this.button4.Text = "INDIETRO";
             this.button4.UseCompatibleTextRendering = true;
@@ -152,11 +147,13 @@
             // 
             // button5
             // 
-            this.button5.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.button5.BackColor = System.Drawing.Color.Transparent;
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button5.Font = new System.Drawing.Font("Arial", 12.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button5.Location = new System.Drawing.Point(381, 85);
+            this.button5.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.button5.Location = new System.Drawing.Point(90, 323);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(153, 68);
+            this.button5.Size = new System.Drawing.Size(121, 48);
             this.button5.TabIndex = 9;
             this.button5.Text = "Cartella da sincronizzare";
             this.button5.UseCompatibleTextRendering = true;
@@ -168,34 +165,109 @@
             // 
             this.label4.AutoSize = true;
             this.label4.BackColor = System.Drawing.Color.Transparent;
-            this.label4.Font = new System.Drawing.Font("Arial", 10.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.ForeColor = System.Drawing.Color.Red;
-            this.label4.Location = new System.Drawing.Point(365, 11);
-            this.label4.MaximumSize = new System.Drawing.Size(170, 0);
+            this.label4.Font = new System.Drawing.Font("Arial", 12.25F);
+            this.label4.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.label4.Location = new System.Drawing.Point(7, 136);
+            this.label4.MaximumSize = new System.Drawing.Size(320, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(170, 48);
+            this.label4.Size = new System.Drawing.Size(159, 19);
             this.label4.TabIndex = 10;
-            this.label4.Text = "INSERISCI TUTTI I CAMPI E SCEGLI LA CARTELLA DA SINCRONIZZARE";
+            this.label4.Text = "Inserisci tutti i campi";
             this.label4.Visible = false;
+            // 
+            // Help
+            // 
+            this.Help.BackColor = System.Drawing.Color.Transparent;
+            this.Help.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Help.Font = new System.Drawing.Font("Arial", 12.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Help.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.Help.Location = new System.Drawing.Point(179, 391);
+            this.Help.Name = "Help";
+            this.Help.Size = new System.Drawing.Size(112, 27);
+            this.Help.TabIndex = 11;
+            this.Help.Text = "Help";
+            this.Help.UseCompatibleTextRendering = true;
+            this.Help.UseVisualStyleBackColor = false;
+            this.Help.Click += new System.EventHandler(this.Help_Click);
+            // 
+            // About
+            // 
+            this.About.BackColor = System.Drawing.Color.Transparent;
+            this.About.FlatAppearance.BorderColor = System.Drawing.Color.DodgerBlue;
+            this.About.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.About.Font = new System.Drawing.Font("Arial", 12.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.About.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.About.Location = new System.Drawing.Point(9, 391);
+            this.About.Name = "About";
+            this.About.Size = new System.Drawing.Size(112, 27);
+            this.About.TabIndex = 12;
+            this.About.Text = "About";
+            this.About.UseCompatibleTextRendering = true;
+            this.About.UseVisualStyleBackColor = false;
+            this.About.Click += new System.EventHandler(this.About_Click);
+            // 
+            // infoLabel
+            // 
+            this.infoLabel.AutoSize = true;
+            this.infoLabel.BackColor = System.Drawing.Color.Transparent;
+            this.infoLabel.Font = new System.Drawing.Font("Arial", 12.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.infoLabel.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.infoLabel.Location = new System.Drawing.Point(5, 117);
+            this.infoLabel.MaximumSize = new System.Drawing.Size(300, 0);
+            this.infoLabel.Name = "infoLabel";
+            this.infoLabel.Size = new System.Drawing.Size(297, 38);
+            this.infoLabel.TabIndex = 13;
+            this.infoLabel.Text = "ddddddddddddddddddddddddddddddddddd";
+            this.infoLabel.Visible = false;
+            // 
+            // portatext
+            // 
+            this.portatext.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.portatext.Font = new System.Drawing.Font("Arial", 10.25F);
+            this.portatext.ForeColor = System.Drawing.SystemColors.InactiveCaption;
+            this.portatext.Location = new System.Drawing.Point(11, 246);
+            this.portatext.Name = "portatext";
+            this.portatext.Size = new System.Drawing.Size(278, 23);
+            this.portatext.TabIndex = 18;
+            this.portatext.Text = "Port Number";
+            this.portatext.Visible = false;
+            this.portatext.MouseClick += new System.Windows.Forms.MouseEventHandler(this.portatext_MouseClick);
+            // 
+            // iptext
+            // 
+            this.iptext.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.iptext.Font = new System.Drawing.Font("Arial", 10.25F);
+            this.iptext.ForeColor = System.Drawing.SystemColors.InactiveCaption;
+            this.iptext.Location = new System.Drawing.Point(11, 281);
+            this.iptext.Name = "iptext";
+            this.iptext.Size = new System.Drawing.Size(278, 23);
+            this.iptext.TabIndex = 19;
+            this.iptext.Text = "Ip address";
+            this.iptext.Visible = false;
+            this.iptext.MouseClick += new System.Windows.Forms.MouseEventHandler(this.iptext_MouseClick);
+            this.iptext.TextChanged += new System.EventHandler(this.iptext_TextChanged);
             // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackgroundImage = global::PrimaGUI.Properties.Resources.sfondoAzzurrino;
+            this.BackgroundImage = global::PrimaGUI.Properties.Resources.x5j8KEF;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(546, 280);
+            this.ClientSize = new System.Drawing.Size(303, 424);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.iptext);
+            this.Controls.Add(this.portatext);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.button5);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.label3);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.button2);
+            this.Controls.Add(this.Help);
+            this.Controls.Add(this.infoLabel);
+            this.Controls.Add(this.About);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button4);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
@@ -211,8 +283,6 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Label label3;
@@ -221,5 +291,10 @@
         private System.Windows.Forms.Button button5;
         private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
         private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button Help;
+        private System.Windows.Forms.Button About;
+        private System.Windows.Forms.Label infoLabel;
+        private System.Windows.Forms.TextBox portatext;
+        private System.Windows.Forms.TextBox iptext;
     }
 }
--- PrimaGUI/PrimaGUI/Form2.cs
+++ PrimaGUI/PrimaGUI/Form2.cs
@@ -19,57 +19,71 @@ namespace PrimaGUI
 
         private void button1_Click(object sender, EventArgs e)
         {
+            this.BackgroundImage = global::PrimaGUI.Properties.Resources._9ToJXnJ;
             button1.Visible = false;
             button2.Visible = false;
             button3.Visible = true;
             button4.Visible = true;
-            label1.Visible = true;
-            label2.Visible = true;
+            Help.Visible = false;
+            About.Visible = false;
             label3.Visible = false;
+            label4.Visible = true;
             textBox1.Visible = true;
             textBox2.Visible = true;
+            iptext.Visible = true;
+
+            portatext.Visible = true;
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
+
+            this.BackgroundImage = global::PrimaGUI.Properties.Resources.rZWcYhh; 
             button1.Visible = false;
             button2.Visible = false;
             button3.Visible = true;
             button4.Visible = true;
+            Help.Visible = false;
+            About.Visible = false;
             button5.Visible = true;
-            label1.Visible = true;
-            label2.Visible = true;
             label3.Visible = false;
+            label4.Visible = true;
             textBox1.Visible = true;
             textBox2.Visible = true;
+            iptext.Visible = true;
+            portatext.Visible = true;
         }
 
         private void button4_Click(object sender, EventArgs e)
         {
+            this.BackgroundImage = global::PrimaGUI.Properties.Resources.x5j8KEF;
             button1.Visible = true;
             button2.Visible = true;
             button3.Visible = false;
             button4.Visible = false;
             button5.Visible = false;
-            label1.Visible = false;
-            label2.Visible = false;
+            Help.Visible = true;
+            About.Visible = true;
             label3.Visible = true;
             label4.Visible = false;
+            infoLabel.Visible = false;
             textBox1.Visible = false;
             textBox2.Visible = false;
+            iptext.Visible = false;
+            portatext.Visible = false;
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
-            if (textBox1.Text == "" || textBox2.Text == ""||(Program.path==""&&button5.Visible==true)) {
+            if (textBox1.Text == "Username" || portatext.Text=="Port Number" || portatext.Text=="Ip address" || textBox2.Text == "Password"||(Program.path==""&&button5.Visible==true)) {
                 if (button5.Visible == false)
                 {
-                    label4.Text = "INSERISCI TUTTI I CAMPI E CONTROLLA CHE SIANO CORRETTI";
+                    label4.Text = "Inserisci tutti i campi";
                 }
                 else {
-                    label4.Text = "INSERISCI TUTTI I CAMPI E SELEZIONA LA CARTELLA DA CONTROLLARE";
+                    label4.Text = "Inserisci tutti i campi";
                 }
-                label4.Visible = true;
+                label4.ForeColor = System.Drawing.Color.Red;
                 return;
             }
             // Questo attualmente è solo uno stub! naturalmente oltre a controllare che siano campi vuoti bisogna fare un check della correttezza
@@ -84,6 +98,91 @@ namespace PrimaGUI
             folderBrowserDialog1.ShowDialog();
             Program.path = folderBrowserDialog1.SelectedPath;
         }
+
+        private void About_Click(object sender, EventArgs e)
+        {
+
+            this.BackgroundImage = global::PrimaGUI.Properties.Resources._9ToJXnJ;
+            button1.Visible = false;
+            button2.Visible = false;
+            button3.Visible = false;
+            button4.Visible = true;
+            button5.Visible = false;
+            Help.Visible = false;
+            About.Visible = false;
+            label3.Visible = false;
+            label4.Visible = false;
+            textBox1.Visible = false;
+            textBox2.Visible = false;
+            iptext.Visible = false;
+            portatext.Visible = false;
+            infoLabel.Visible = true;
+            infoLabel.Text = "Questo programma è stato sviluppato interamente da Paolo Gallina come progetto del corso di Programmazione di Sistema\n\nIl core del programma è scritto in c++ la gui in c#, sono state utilizzate librerie open source di terze parti per calcolare l'hash e per gestire il database interno.\n\nL'intento è creare un servizio di backup e versioning automatico di una cartella che permetta di recuperare versioni passate o file cancellati.";
+        }
+
+        private void Help_Click(object sender, EventArgs e)
+        {
+
+            this.BackgroundImage = global::PrimaGUI.Properties.Resources._9ToJXnJ;
+            button1.Visible = false;
+            button2.Visible = false;
+            button3.Visible = false;
+            button4.Visible = true;
+            button5.Visible = false;
+            Help.Visible = false;
+            About.Visible = false;
+            label3.Visible = false;
+            label4.Visible = false;
+            textBox1.Visible = false;
+            textBox2.Visible = false;
+            iptext.Visible = false;
+            portatext.Visible = false;
+            infoLabel.Visible = true;
+            infoLabel.Text = "Stai visualizzando la procedura di login del programma.\n\nSe non lo hai già fatto puoi procedere alla registrazione attraverso il corrispondente pannello scegliendo uno username, una password e la cartellla da sincronizzare.\n\nAl contrario puoi procedere al login.\n\nSe hai dimenticato le credenziali contatta lo Sviluppatore.";
+        }
+
+        private void textBox1_TextChanged(object sender, MouseEventArgs e)
+        {
+            if (textBox1.Text == "Username")
+            {
+                textBox1.Text = "";
+                textBox1.ForeColor = SystemColors.WindowText;
+            }
+        }
+
+        private void textBox2_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (textBox2.Text == "Password")
+            {
+                textBox2.Text = "";
+                textBox2.UseSystemPasswordChar = true;
+                textBox2.ForeColor = SystemColors.WindowText;
+            }
+        }
+
+        private void portatext_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (portatext.Text == "Port Number")
+            {
+                portatext.Text = "";
+                portatext.ForeColor = SystemColors.WindowText;
+            }
+        }
+
+        private void iptext_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (iptext.Text == "Ip address")
+            {
+                iptext.Text = "";
+                iptext.ForeColor = SystemColors.WindowText;
+            }
+        }
+
+        private void iptext_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
 
 
     
